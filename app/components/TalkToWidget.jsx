206cad893397bad8e318dbb0bf516cff2d997449
noah <noaligpitan26@gmail.com>
2025-04-15 12:09:45 +0800

    add tawk.to v1

--- app/components/TalkToWidget.jsx
+++ app/components/TalkToWidget.jsx
@@ -1,20 +1,14 @@
 "use client";
 
+import Script from "next/script";
 import { useEffect } from "react";
 
 export default function TawkToWidget() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://embed.tawk.to/67fdd6c4103aa0190d9e8593/default";
-    script.async = true;
-    script.charset = "UTF-8";
-    script.setAttribute("crossorigin", "*");
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  return null;
+  <>
+    <Script
+      src="https://embed.tawk.to/67fdd6c4103aa0190d9e8593/default"
+      strategy="afterInteractive"
+      crossOrigin="*"
+    />
+  </>;
 }
